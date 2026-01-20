548cd5f7393208d2a92edc4e9d71d195302d1683
PHilado <32675551+Dcroix@users.noreply.github.com>
2026-01-20 22:14:02 +0800

    Add files via upload

---
+++ readsqldataR.R
@@ -0,0 +1,20 @@
+library(RPostgreSQL) # Load Necessary Libraries
+library(RPostgres)
+library(DBI)
+
+con <- dbConnect( # Establish Database Connection
+  RPostgres::Postgres(),
+  dbname = "xxxxxxxxxxxx",
+  host = "xxxxxxxxxxxxxxx",
+  port = xxxx,
+  user = "xxxxxx",
+  password = "xxxxxxxx"
+)
+
+# Read the table into a data frame
+coh_data <- dbReadTable(con, "coh") # Read table and store on said variable
+
+# View the first few rows
+head(coh_data) 
+# Database Disconnect
+dbDisconnect(con)
